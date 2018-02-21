b4d97db58d8e4164f4869910e696765808b260fd
Kevin <klambe17@gmail.com>
2018-02-21 14:44:38 +0000

    changed mongo to local version not container version

--- simple_reverse_proxy/server/db/mongoose.js
+++ simple_reverse_proxy/server/db/mongoose.js
@@ -4,10 +4,10 @@ mongoose.Promise = global.Promise;
 // mongoose.connect(process.env.MONGODB_URI ||'mongodb:27017/cache'); //issue with docker image crashing
 
 //local host docker test
-mongoose.connect('mongodb://mongo:27017/cache');
+// mongoose.connect('mongodb://mongo:27017/cache');
 
 //local host test
-// mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/cache');
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/cache');
 
 
 module.exports = {mongoose};
